aa7e1eb52c1bb693588698a7e906e6dc2dd36c28
atSingapore <anya.traille@gmail.com>
2021-04-07 00:09:33 +0800

    Second Commit

---
+++ app/src/main/java/com/udacity/LoadingFragment.kt
@@ -0,0 +1,36 @@
+package com.udacity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.udacity.databinding.ContentMainBinding
+
+class LoadingFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: ContentMainBinding = DataBindingUtil.inflate(
+            inflater, R.layout.content_main, container, false
+        )
+
+        val viewModel = ViewModelProviders.of(this).get(LoadingViewModel::class.java)
+
+        binding.loadingViewModel = viewModel
+        binding.lifecycleOwner = this.viewLifecycleOwner
+
+        return binding.root
+    }
+
+    companion object {
+        fun newInstance() = LoadingFragment()
+    }
+
+}
---
+++ app/src/main/java/com/udacity/LoadingViewModel.kt
@@ -0,0 +1,22 @@
+package com.udacity
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class LoadingViewModel(private val app: Application) : AndroidViewModel(app) {
+
+    fun onGlideSelected()
+    {
+
+    }
+
+    fun onLoadAppSelected()
+    {
+
+    }
+
+    fun onRetrofitSelected()
+    {
+
+    }
+}
