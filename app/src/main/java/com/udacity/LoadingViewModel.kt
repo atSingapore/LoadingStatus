d8938ec4251dcc546ba56b9afd70eda7c93bb092
atSingapore <anya.traille@gmail.com>
2021-04-07 00:27:09 +0800

    Radio Buttons Working

--- app/src/main/java/com/udacity/LoadingViewModel.kt
+++ app/src/main/java/com/udacity/LoadingViewModel.kt
@@ -1,21 +1,22 @@
 package com.udacity
 
 import android.app.Application
+import android.view.View
 import androidx.lifecycle.AndroidViewModel
 
 class LoadingViewModel(private val app: Application) : AndroidViewModel(app) {
 
-    fun onGlideSelected()
+    fun onGlideSelected(view: View)
     {
 
     }
 
-    fun onLoadAppSelected()
+    fun onLoadAppSelected(view: View)
     {
 
     }
 
-    fun onRetrofitSelected()
+    fun onRetrofitSelected(view: View)
     {
 
     }
