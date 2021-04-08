fea5f8122552d836b7cabd0a9c12177a9f99f7bb
atSingapore <anya.traille@gmail.com>
2021-04-08 14:56:17 +0800

    Don't need LoadingFragment either

--- app/src/main/java/com/udacity/LoadingFragment.kt
+++ app/src/main/java/com/udacity/LoadingFragment.kt
@@ -9,28 +9,28 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.udacity.databinding.ContentMainBinding
 
-class LoadingFragment : Fragment() {
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        val binding: ContentMainBinding = DataBindingUtil.inflate(
-            inflater, R.layout.content_main, container, false
-        )
-
-        val viewModel = ViewModelProviders.of(this).get(LoadingViewModel::class.java)
-
-        binding.loadingViewModel = viewModel
-        binding.lifecycleOwner = this.viewLifecycleOwner
-
-        return binding.root
-    }
-
-    companion object {
-        fun newInstance() = LoadingFragment()
-    }
-
-}
+//class LoadingFragment : Fragment() {
+//
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//
+//        val binding: ContentMainBinding = DataBindingUtil.inflate(
+//            inflater, R.layout.content_main, container, false
+//        )
+//
+//        val viewModel = ViewModelProviders.of(this).get(LoadingViewModel::class.java)
+//
+//        binding.loadingViewModel = viewModel
+//        binding.lifecycleOwner = this.viewLifecycleOwner
+//
+//        return binding.root
+//    }
+//
+//    companion object {
+//        fun newInstance() = LoadingFragment()
+//    }
+//
+//}
